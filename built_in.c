1a35c07c09b5fcdbc24a1e76fbceaf914ce3af56
Aaron Mueller <ammaaronmueller@gmail.com>
2017-04-01 18:45:53 -0400

    built_in.c  built_in.h	built_in.o  Makefile  nsh  nsh.c  nsh.o  README.md functionality/comments

--- built_in.c
+++ built_in.c
@@ -5,12 +5,15 @@ void set(char* tokens[]) {}
 
 void prompt(char* tokens[]) {}
 
+// use `chdir()` to change directory; handle errors here
 void dir(char* tokens[]) {
+	// if no argument, print error message to stderr
 	if (!tokens[1]) {
 		fprintf(stderr, "\'dir\' usage: dir <directory>");
 		return;
 	}
 	
+	// if chdir does not return 0, print error message using perror()
 	if (chdir(tokens[1]) != 0) {
 		perror(tokens[1]);
 	}

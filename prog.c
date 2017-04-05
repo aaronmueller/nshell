02655995687478c77b1ef84f919e9590299ea853
canim44 <connorcanim@gmail.com>
2017-04-05 01:34:36 -0400

    Removed extra prog. safensh.c is old tokenize

--- prog.c
+++
@@ -1,9 +0,0 @@
-#include <string.h>
-#include <stdio.h>
-#include <stdlib.h>
-int main(){
-	char* thing = "this is a \"test\"";
-	char* brk = strtok(thing, "\"");
-	printf("%s", brk);
-	return 0;
-}
